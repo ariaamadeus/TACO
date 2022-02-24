4b0fb3cf9c5f63e2dd01c92d2a2b48bc78486438
Aria Amadeus <amadeus.aria@gmail.com>
2022-02-24 22:07:07 +0700

    Direct Convert TACO .json to VOC .xml

--- detector/split_dataset.py
+++ detector/split_dataset.py
@@ -1,16 +1,21 @@
 import os.path
+import random
+import copy
+
 import json
 import argparse
+
 import numpy as np
-import random
 import datetime as dt
-import copy
+
+import xmlConv #JetsonXMLHandler()
 
 parser = argparse.ArgumentParser(description='User args')
 parser.add_argument('--dataset_dir', required=True, help='Path to dataset annotations')
 parser.add_argument('--test_percentage', type=int, default=10, required=False, help='Percentage of images used for the testing set')
 parser.add_argument('--val_percentage', type=int, default=10, required=False, help='Percentage of images used for the validation set')
 parser.add_argument('--nr_trials', type=int, default=10, required=False, help='Number of splits')
+parser.add_argument('--convert', type=str, default='no', required=False, help='Convert to another trainable annotations')
 
 args = parser.parse_args()
 
@@ -28,6 +33,32 @@ scene_anns = dataset['scene_annotations']
 
 #nr_testing_images = int(nr_images*args.test_percentage*0.01+0.5)
 #nr_nontraining_images = int(nr_images*(args.test_percentage+args.val_percentage)*0.01+0.5)
+
+# Internal Rename
+theBatch = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+for x in dataset['images']:
+    splited = x['file_name'].split('/')
+    theBatchNum = int((splited[0]).replace('batch_',''))
+    theBatch[theBatchNum].append(splited[1])
+    
+for x in theBatch:
+    x.sort()
+
+countUp = 0 #Join all batch from batch_0/000000.JPG
+batchIndex = 0
+for nImages in theBatch:
+    for nImage in nImages:
+        for image in dataset['images']: #Dirty way to find, or maybe the best idk
+            if image['file_name'] == "batch_"+str(batchIndex)+'/'+nImage:
+                strCU = str(countUp)
+                countUp+=1
+                reName = ""
+                for y in range(0,6-len(strCU)):
+                    reName += "0"
+                reName += strCU+".jpg"
+                image['file_name'] = reName
+                break
+    batchIndex += 1
 
 # The real split dataset
 picked_cat = ["Other plastic bottle", "Clear plastic bottle", "Disposable plastic cup", 
@@ -116,11 +147,71 @@ for i in range(args.nr_trials):
     ann_val_out_path   = args.dataset_dir + '/' + 'annotations_' + str(i) + '_val.json'
     ann_test_out_path  = args.dataset_dir + '/' + 'annotations_' + str(i) + '_test.json'
 
-    with open(ann_train_out_path, 'w+') as f:
-        f.write(json.dumps(train_set))
+    if args.convert == "no":
+        print('writing...')
+        with open(ann_train_out_path, 'w+') as f:
+            f.write(json.dumps(train_set))
 
-    with open(ann_val_out_path, 'w+') as f:
-        f.write(json.dumps(val_set))
+        with open(ann_val_out_path, 'w+') as f:
+            f.write(json.dumps(val_set))
 
-    with open(ann_test_out_path, 'w+') as f:
-        f.write(json.dumps(test_set))
+        with open(ann_test_out_path, 'w+') as f:
+            f.write(json.dumps(test_set))
+            
+    elif args.convert == "voc":
+        print('converting...')
+        for image in train_set['images']:
+            converter = JetsonXMLHandler()
+            converter.changeName(image["file_name"])
+            converter.changeSize(str(image["width"]),str(image["height"]))
+            for annotate in train_set['annotations']:
+                if annotate['image_id'] == image['id']:
+                    rawbbox = annotate['bbox']
+                    bbox = (rawbbox[0],
+                            rawbbox[1],
+                            rawbbox[0]+rawbbox[2],
+                            rawbbox[1]+rawbbox[3])
+
+                    for label in val_set['categories']:
+                        if label['id'] == annotate['category_id']:
+                            Label = label['name']
+                    converter.createObject(Label, bbox)
+            converter.writeXML('train')
+        
+        for image in val_set['images']:
+            converter = JetsonXMLHandler()
+            converter.changeName(image["file_name"])
+            converter.changeSize(str(image["width"]),str(image["height"]))
+            for annotate in val_set['annotations']:
+                if annotate['image_id'] == image['id']:
+                    rawbbox = annotate['bbox']
+                    bbox = (rawbbox[0],
+                            rawbbox[1],
+                            rawbbox[0]+rawbbox[2],
+                            rawbbox[1]+rawbbox[3])
+                    for label in val_set['categories']:
+                        if label['id'] == annotate['category_id']:
+                            Label = label['name']
+                    converter.createObject(Label, bbox)
+            converter.writeXML('val')
+
+        for image in test_set['images']:
+            converter = JetsonXMLHandler()
+            converter.changeName(image["file_name"])
+            converter.changeSize(str(image["width"]),str(image["height"]))
+            for annotate in test_set['annotations']:
+                if annotate['image_id'] == image['id']:
+                    rawbbox = annotate['bbox']
+                    for label in val_set['categories']:
+                        if label['id'] == annotate['category_id']:
+                            Label = label['name']
+                    bbox = (rawbbox[0],
+                            rawbbox[1],
+                            rawbbox[0]+rawbbox[2],
+                            rawbbox[1]+rawbbox[3])
+                    converter.createObject(Label, bbox)
+            converter.writeXML('test')  
+            
+        print('We just do it once for voc')
+        break
+print('Done') 
